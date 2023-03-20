d4d488225b54c24ffe41ace390f4cd98ae486612
inevitable360 <pvidalx@gmail.com>
2023-03-20 11:29:21 +0000

    Fix Browser Storage Issues
    Fixed the reload page to check local storage because browsers needs some miliseconds to get the local storage correctly

--- dogefy.js
+++ dogefy.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
+
+    // we test if you have the Browser local Storage enable to store the Fiat Value of Dogecoin to calculate prices
+    function isLocalStorageAvailable(){
+        var SuchTest = 'SuchTest';
+        try {
+            localStorage.setItem(SuchTest, SuchTest);
+            localStorage.removeItem(SuchTest);
+            return true;
+        } catch(e) {
+            return false;
+        }
+    }
     
+    if(!isLocalStorageAvailable()){
+        alert("Please enable Local Storage on your browser to be able to store the Dogecoin current value");
+    }
+
     // we get the current fiat value of Dogecoin to be able to convert your website prices to Doge
     $.getJSON("https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=" + fiat_currency, function(data){
         localStorage.setItem('dogecoinValue', data["dogecoin"][fiat_currency]); // we store the value in local storage        
@@ -9,9 +25,13 @@ $(document).ready(function() {
     });
 
     // until we cant get the local storage dogecoin value do to JQuery Storage, we reload the webpage
-    var dogecoinValue = localStorage.getItem('dogecoinValue');
-    if (!$.isNumeric(dogecoinValue)){
-        location.reload();
+    const dogecoinValue = localStorage.getItem('dogecoinValue');
+    if (dogecoinValue <= 0 ){
+        setTimeout(
+        function() 
+            {
+              location.reload();
+            }, 120);
     };
 
     // now we try to find all fiat prices on your webpage
@@ -27,8 +47,7 @@ $(document).ready(function() {
         // if we find a currency value we try to convert into Dogecoin 
         if (matches) {
             for (var i = 0; i < matches.length; i++) {
-                fiat = parseFloat(matches[i].match(/[\d\.]+/)); // we get numeric value
-
+                fiat = (matches[i].match(/[\d\.]+/)); // we get numeric value
                 //we save all HTML to dogefy the webpage
                 dogefy = $('body').html().replace(fiat_currency_symbol + fiat, 'Ð' + (fiat / dogecoinValue).toFixed(2));
 
